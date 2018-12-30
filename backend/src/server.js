b51b901a09620cf67237bad581dec33e982aa9bf
Bogacki Andrzej <aboga@dfds.com>
2018-12-30 14:05:22 +0100

    Dockerizes mongo database, composes all on build, creates jmeterui network

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ keystone.init({
   // or run transformation scripts against your database.
   'auto update': true,
   // The url for your MongoDB connection
-  'mongo': 'mongodb://localhost:27017/jmeteruidb',
+  'mongo': 'mongodb://mongodb:27017/db',
   // Whether to enable built-in authentication for Keystone's Admin UI,
   'auth': true,
   // The key of the Keystone List for users, required if auth is set to true
