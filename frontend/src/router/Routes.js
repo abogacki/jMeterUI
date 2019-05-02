70b389f70ac9fdb85585391952950e2b2e3943cd
aboga <aboga@MacBook-Pro-aboga.local>
2019-05-02 11:38:37 +0200

    Adds lazy/suspense codesplitting to Routes

--- frontend/src/router/Routes.js
+++ frontend/src/router/Routes.js
@@ -1,35 +1,42 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Route } from "react-router-dom";
-import Home from "../react/views/Home";
-import Tests from "../react/views/testsList/TestsList";
-import About from "../react/views/About";
-import Details from "../react/views/details/Details";
-import CreateTest from "../react/views/TestCreateView";
-import HeroNavbarWrapper from "./HeroNavbarWrapper";
+
+const Home = lazy(() => import("../react/views/Home"));
+const Tests = lazy(() => import("../react/views/testsList/TestsList"));
+const About = lazy(() => import("../react/views/About"));
+const Details = lazy(() => import("../react/views/details/Details"));
+const CreateTest = lazy(() => import("../react/views/TestCreateView"));
+const HeroNavbarWrapper = lazy(() => import("./HeroNavbarWrapper"));
 
 const WrappedHome = () => <HeroNavbarWrapper component={Home} />;
+
 const WrappedTests = () => (
-  <HeroNavbarWrapper title={"Tests"} component={Tests} />
+  <HeroNavbarWrapper title="Tests" component={Tests} />
 );
+
 const WrappedDetails = () => (
-  <HeroNavbarWrapper title={"Details"} component={Details} />
+  <HeroNavbarWrapper title="Details" component={Details} />
 );
+
 const WrappedAbout = () => (
-  <HeroNavbarWrapper title={"About"} component={About} />
+  <HeroNavbarWrapper title="About" component={About} />
 );
+
 const WrappedCreateTest = () => (
-  <HeroNavbarWrapper title={"Create test"} component={CreateTest} />
+  <HeroNavbarWrapper title="Create test" component={CreateTest} />
 );
+
+const Loader = () => <div className="Loader" />;
 
 const Routes = () => {
   return (
-    <>
+    <Suspense fallback={<Loader />}>
       <Route exact path="/" component={WrappedHome} />
       <Route exact path="/tests/create" component={WrappedCreateTest} />
       <Route exact path="/tests" component={WrappedTests} />
       <Route path="/details/:testId" component={WrappedDetails} />
       <Route path="/about" component={WrappedAbout} />
-    </>
+    </Suspense>
   );
 };
 
